c2d03df477f53cf789629929299d621716093626
a <a@PEAK2.local>
2023-06-13 22:38:38 +0100

    update

---
+++ models/cart.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    //fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      //Analyze the cart => Check existing product
+      const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+      const existingProduct = cart.products[existingProductIndex]
+      let updatedProduct;
+      //Add new product/increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct]
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
+      })
+    });
+  }
+};
+
+
+//The reason i used " err " to test the existing product is that, reading/searching for a particular 
+//product from this file using ***" fs.readFile(p, (err, fileContent) "*** by consologging the two arguments
+//console.log(fileContent) wil displayed all d saved product insyd d path wch means file exists already while
+//console.log(err) will tell you no such file in the directory, means file doesnt exist yet
+//That means, fileContent indicates that there's an existing product and err indicates that such product is 
+//not there, wch means that you can create a new product
